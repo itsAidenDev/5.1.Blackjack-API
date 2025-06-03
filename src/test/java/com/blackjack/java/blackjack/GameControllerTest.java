4c46a3f3936ed8b4a79e26effb99a99567008a7d
Aiden <yumenushii.dev@gmail.com>
2025-06-03 18:28:48 +0200

    More bugs fixed

--- src/test/java/com/blackjack/java/blackjack/GameControllerTest.java
+++ src/test/java/com/blackjack/java/blackjack/GameControllerTest.java
@@ -1,11 +1,13 @@
 package com.blackjack.java.blackjack;
 
+
 import com.blackjack.java.blackjack.controllers.GameController;
 import com.blackjack.java.blackjack.dto.CreateGameRequestDTO;
 import com.blackjack.java.blackjack.dto.GameResponseDTO;
 import com.blackjack.java.blackjack.dto.PlayerDTO;
-import com.blackjack.java.blackjack.service.GameService;
 import com.blackjack.java.blackjack.utils.GameStatus;
+import com.blackjack.java.blackjack.service.GameService;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,15 +15,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+
 import java.util.List;
-import static org.mockito.ArgumentMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class GameControllerTest {
+
+    private WebTestClient client;
 
     @Mock
     private GameService gameServiceMock;
@@ -29,11 +32,7 @@ class GameControllerTest {
     @InjectMocks
     private GameController gameController;
 
-    private WebTestClient client;
-
     private GameResponseDTO sampleResponse;
-    private CreateGameRequestDTO createGameRequest;
-    private PlayerDTO playerTest;
 
     @BeforeEach
     void setUp() {
@@ -42,39 +41,33 @@ class GameControllerTest {
                 .baseUrl("/game")
                 .build();
 
-        playerTest = new PlayerDTO(78L, "Peter", 250);
+        PlayerDTO player = new PlayerDTO(1L, "Fernando", 500);
         sampleResponse = new GameResponseDTO(
-                2L,
-                playerTest,
+                "gameId",
+                player,
                 List.of(),
                 List.of(),
                 GameStatus.IN_PROGRESS,
                 true,
-                34
-        );
-
-        createGameRequest = new CreateGameRequestDTO(78L, 75);
+                52);
     }
 
     @Test
     void createGame_Returns201AndBody() {
-        Mockito.when(gameServiceMock.createGame(anyLong(), anyInt()))
+        CreateGameRequestDTO request = new CreateGameRequestDTO(1L, 100);
+        Mockito.when(gameServiceMock.createGame(request.getPlayerId(), request.getBet()))
                 .thenReturn(Mono.just(sampleResponse));
-        WebTestClient.ResponseSpec response = client.post()
-                .uri("/new/1")
+
+        client.post()
+                .uri("/new")
                 .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(createGameRequest)
-                .exchange();
-        response.expectStatus().isEqualTo(HttpStatus.CREATED.value())
+                .bodyValue(request)
+                .exchange()
+                .expectStatus().isCreated()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBody()
-                .jsonPath("$.gameId").isEqualTo(sampleResponse.getGameId())
-                .jsonPath("$.player.id").isEqualTo(playerTest.getPlayerId())
-                .jsonPath("$.status").isEqualTo(sampleResponse.getStatus().getStatusName());
-    }
-
-    @Test
-    void createGame_InvalidRequest_Returns400() {
-        // Add test for invalid request
+                .jsonPath("$.gameId").isEqualTo("gameId")
+                .jsonPath("$.player.id").isEqualTo(1)
+                .jsonPath("$.status").isEqualTo("IN_PROGRESS");
     }
 }
